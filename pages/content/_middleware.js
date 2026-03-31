cf26bc977b216808280b3a01b8d527165bb79658
psychoinhumain <psycho.inhumain@gmail.com>
2026-03-31 21:41:51 +0200

    Delete pages/content/_middleware.js

--- pages/content/_middleware.js
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-export async function middleware(req, res) {
-  const { nextUrl: url, geo } = req;
-  url?.searchParams?.set("country", geo.country ?? "US");
-  return NextResponse.rewrite(url);
-}
