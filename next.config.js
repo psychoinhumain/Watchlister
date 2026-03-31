2cd0c176bfae96332bc56f74ad3e1bf8a00ad563
psychoinhumain <psycho.inhumain@gmail.com>
2026-03-31 21:37:04 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
+
+module.exports = nextConfig
